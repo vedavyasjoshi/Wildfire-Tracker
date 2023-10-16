c9d532e3039d3ce5649b41319f35c4d5bde2c17b
Vedavyas <vedavyasj@juniper.net>
2023-10-16 15:12:08 +0530

    fix: Update the URL

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import {useState, useEffect } from 'react'
-import Map from './components/Map'
-import Loader from './components/Loader'
-import Header from './components/Header'
+import { useState, useEffect } from "react";
+import Map from "./components/Map";
+import Loader from "./components/Loader";
+import Header from "./components/Header";
 
 function App() {
-const[eventData,setEventData] = useState([])
-const[loading,setLoading] = useState(false)
+  const [eventData, setEventData] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-useEffect(()=>{
-  const fetchEvents =async() =>{
-    setLoading(true)
-    const res =await fetch ("https://eonet.sci.gsfc.nasa.gov/api/v2.1/events")
-    const{events} = await res.json()
-    setEventData(events)
-    setLoading(false)
-  }
-  fetchEvents()
-},[])
+  useEffect(() => {
+    const fetchEvents = async () => {
+      setLoading(true);
+      const apiURL = "https://eonet.gsfc.nasa.gov/api/v3/events";
+      const res = await fetch(apiURL);
+      const { events } = await res.json();
+      setEventData(events);
+      setLoading(false);
+    };
+    fetchEvents();
+  }, []);
 
   return (
     <div>
       <Header />
-    {!loading?<Map eventData={eventData} />: <Loader />}
+      {!loading ? <Map eventData={eventData} /> : <Loader />}
     </div>
   );
 }
